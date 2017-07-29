1ff2b5cb757425cf74b9f072499d93bc8f38384f
Jake Pogue <bb2jake@gmail.com>
2017-07-28 21:35:56 -0600

    Set up the status bar.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -33,9 +33,9 @@ var hero = new Combatant(
     "assets/art/hero.png"
 );
 
-var lionMouse = new Combatant (
+var lionMouse = new Combatant(
     "Lion Mouse",
-    new Stats (50, 7, 2),
+    new Stats(50, 7, 2),
     [
         new Attack("Bite", 1, 1, 0),
         new Attack("Tail Swipe", 1, 0.5, 10),
@@ -44,24 +44,80 @@ var lionMouse = new Combatant (
     "assets/art/lionMouse.png"
 )
 
-function drawCombatants () {
+var currentEnemy = lionMouse;
+
+function update() {
     let template = '';
-    
+
     template += `
         <div id="hero" class="hero text-center">
-            <div id="attack-buttons">
-                <button class="btn" onclick="">${hero.attacks[0].name}</button>
-                <button class="btn" onclick="">${hero.attacks[1].name}</button>
-                <button class="btn" onclick="">${hero.attacks[2].name}</button>
-            </div>
             <img src=${hero.image} alt="hero image">
         </div>
         <div id="enemy1" class="enemy">
-            <img src=${lionMouse.image} alt="enemy image">
+            <img src=${currentEnemy.image} alt="enemy image">
         </div>
     `
 
     document.getElementById('combatants').innerHTML = template;
+    drawHeroStatusBar();
+    drawEnemyStatusBar();
 }
 
-drawCombatants();
+function drawHeroStatusBar() {
+    let template = `
+        <div id="heroName">
+            <h3>Name: ${hero.name}</h3>
+        </div>
+        <div id="heroHpText">
+            <h3>HP: ${hero.stats.currentHp}/${hero.stats.maxHp}</h3>
+        </div>
+        <div id="attack-buttons">
+            <button class="" type="button" onpointerenter="" onpointerleave="" onclick="heroAttack(0)">${hero.attacks[0].name}</button>
+            <button class="" type="button" onpointerenter="" onpointerleave="" onclick="heroAttack(1)">${hero.attacks[1].name}</button>
+            <button class="" type="button" onpointerenter="" onpointerleave="" onclick="heroAttack(2)">${hero.attacks[2].name}</button>
+        </div>
+    `
+
+    document.getElementById('hero-status-bar').innerHTML = template;
+}
+
+function drawEnemyStatusBar() {
+    let template = `
+        <div id="enemyName">
+            <h3>Name: ${currentEnemy.name}</h3>
+        </div>
+        <div id="enemyHpText">
+            <h3>HP: ${currentEnemy.stats.currentHp}/${currentEnemy.stats.maxHp}</h3>
+        </div>
+    `
+
+    document.getElementById('enemy-status-bar').innerHTML = template;
+}
+
+function heroAttack(attackNum) {
+    if (hero.stats.currentHp > 0 && currentEnemy && currentEnemy.stats.currentHp > 0) {
+        attack(hero, attackNum, currentEnemy);
+        enemyAttack();
+    }
+}
+
+function enemyAttack() {
+    let rnd = Math.floor(Math.random() * currentEnemy.attacks.length);
+    attack(currentEnemy, rnd, hero);
+}
+
+function attack(attacker, attackNum, target) {
+    target.stats.currentHp -= attacker.attacks[attackNum].potency * attacker.stats.atk;
+    if (target.stats.currentHp < 0) target.stats.currentHp = 0;
+
+    update();
+}
+
+function hardReset() {
+    hero.stats.currentHp = hero.stats.maxHp;
+    currentEnemy.stats.currentHp = currentEnemy.stats.maxHp;
+
+    update();
+}
+
+update();
