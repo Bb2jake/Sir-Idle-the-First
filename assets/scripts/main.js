baa6c97d28c71be8073e55d93f9024ff15c947ae
Jake Pogue <bb2jake@gmail.com>
2017-07-30 21:02:41 -0600

    Added leveling, fighting new enemies and new zones.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,15 +24,31 @@ function Combatant(name, stats, attacks, image) {
 function Hero(name, stats, attacks, image, potions) {
     Combatant.call(this, name, stats, attacks, image);
     this.potions = potions;
+    this.level = 1;
+    this.currentExp = 0;
+    this.expToLevel = 10;
 
     this.getAtk = function () {
-        let potion = potions['atkPotion'];
-        return potion.isActive ? stats.atk + stats.atk * potion.boostPercent / 100 : stats.atk;
+        let potion = this.potions['atkPotion'];
+        return potion.isActive ? this.stats.atk + this.stats.atk * potion.boostPercent / 100 : this.stats.atk;
     }
 
     this.getSpd = function () {
-        let potion = potions['spdPotion'];
-        return potion.isActive ? stats.spd + stats.spd * potion.boostPercent / 100 : stats.spd;
+        let potion = this.potions['spdPotion'];
+        return potion.isActive ? this.stats.spd + this.stats.spd * potion.boostPercent / 100 : this.stats.spd;
+    }
+
+    this.checkLevel = function () {
+        while (this.currentExp > this.expToLevel) {
+            this.level++;
+            this.stats.maxHp = Math.floor(this.stats.maxHp * 1.1);
+            this.stats.currentHp = this.stats.maxHp;
+            this.stats.atk = Math.floor(this.stats.atk * 1.1);
+            this.stats.spd = Math.floor(this.stats.spd * 1.1);
+
+            this.currentExp -= this.expToLevel;
+            this.expToLevel = this.level * 10;
+        }
     }
 }
 
@@ -46,40 +62,91 @@ function Potion(name, boostStat, boostPercent, boostTime, quantity, image) {
     this.isActive = false;
 }
 
+function Zone(name, enemyMult, image) {
+    this.name = name;
+    this.enemyMult = enemyMult;
+    this.image = image;
+}
+
+var zones = [
+    new Zone("Prairie", 1, "assets/art/prairieBG.png"),
+    new Zone("Prairie", 1.5, "assets/art/forestBG.png"),
+    new Zone("Prairie", 2, "assets/art/caveBG.png"),
+]
+
 // Fighters //
-var hero = new Hero(
-    "Kai",
-    new Stats(100, 10, 1),
+var hero;
+
+function createHero() {
+    hero = new Hero(
+        "Kai",
+        new Stats(100, 10, 1),
+        [
+            new Attack("Slash", 1, 1, 0, "Slash with your sword."),
+            new Attack("Leg Sweep", 1, 0.5, 10, "Sweep the leg! Resets opponents attack and stuns for 2 seconds.", true),
+            new Attack("Fireball", 3, 2, 15, "Throw a fireball at your opponent.")
+        ],
+        "assets/art/hero.png",
+        {
+            hpPotion: new Potion("Healing Potion", "currentHp", 25, 0, 3, "assets/art/healthPotion.png"),
+            atkPotion: new Potion("Attack Potion", "atk", 25, 20, 3, "assets/art/attackPotion.png"),
+            spdPotion: new Potion("Speed Potion", "spd", 25, 20, 3, "assets/art/speedPotion.png")
+        }
+    );
+}
+
+createHero();
+
+var enemies = [
+    new Combatant(
+        "Lion Mouse",
+        new Stats(50, 7, 2),
+        [
+            new Attack("Bite", 1, 1, 0),
+            new Attack("Tail Swipe", 1, 0.5, 10, "", true),
+            new Attack("Body Slam", 3, 2, 15)
+        ],
+        "assets/art/lionMouse.png"),
+    new Combatant(
+        "Harpy",
+        new Stats(100, 15, 1.5),
+        [
+            new Attack("Claw", 1, 1, 0),
+            new Attack("Scream", 1, 0.5, 10, "", true),
+            new Attack("Dive Bomb", 3, 2, 15)
+        ],
+        "assets/art/harpy.png"),
+    new Combatant(
+        "Baby Hydra",
+        new Stats(300, 10, 1),
+        [
+            new Attack("Bite", 1, 1, 0),
+            new Attack("Tail Swipe", 1, 0.5, 10, "", true),
+            new Attack("Fire Breath", 3, 2, 15)
+        ],
+        "assets/art/hydra.png"
+    )
+];
+
+var boss = new Combatant(
+    "Prairie King",
+    new Stats(500, 20, 2),
     [
-        new Attack("Slash", 1, 1, 0, "Slash with your sword."),
-        new Attack("Leg Sweep", 1, 0.5, 10, "Sweep the leg! Resets opponents attack and stuns for 2 seconds.", true),
-        new Attack("Fireball", 3, 2, 15, "Throw a fireball at your opponent.")
+        new Attack("Gnaw", 1, 1, 0),
+        new Attack("Howl", 1, 0.5, 10, "", true),
+        new Attack("Plague Breath", 3, 2, 15)
     ],
-    "assets/art/hero.png",
-    {
-        atkPotion: new Potion("Attack Potion", "atk", 25, 20, 3, "assets/art/attackPotion.png"),
-        spdPotion: new Potion("Speed Potion", "spd", 25, 20, 3, "assets/art/speedPotion.png"),
-        hpPotion: new Potion("Healing Potion", "currentHp", 25, 0, 3, "assets/art/healthPotion.png")
-    }
+    "assets/art/prairieKing.png"
 );
 
-var lionMouse = new Combatant(
-    "Lion Mouse",
-    new Stats(50, 7, 2),
-    [
-        new Attack("Bite", 1, 1, 0),
-        new Attack("Tail Swipe", 1, 0.5, 10, "", true),
-        new Attack("Body Slam", 3, 2, 15)
-    ],
-    "assets/art/lionMouse.png"
-)
-
-var currentEnemy = lionMouse;
+var currentEnemy = enemies[0];
+var currentZone = zones[0];
 
 function update() {
     drawSprites();
     drawHeroStatusBar();
     drawEnemyStatusBar();
+    document.getElementById("background").style.backgroundImage = `url(${currentZone.image})`;
 }
 
 function drawSprites() {
@@ -102,20 +169,21 @@ function drawHeroStatusBar() {
         <div id="hero-stats" class="col-xs-6 standardBG">
             <h4>Name: ${hero.name}</h4>
             <p>HP: <span class="standardBG">${hero.stats.currentHp}/${hero.stats.maxHp}</span></p>
+            <p>LV: ${hero.level} | Exp: ${hero.currentExp}/${hero.expToLevel}</p>
             <p>ATK: ${hero.getAtk()}</p>
             <p>SPD: ${hero.getSpd()}</p>
         </div>
         <div id="attack-buttons" class="col-xs-3 standardBG">
-            <button class="" type="button" onpointerenter="" onpointerleave="" onclick="heroAttack(0)">${hero.attacks[0].name}</button>
-            <button class="" type="button" onpointerenter="" onpointerleave="" onclick="heroAttack(1)">${hero.attacks[1].name}</button>
-            <button class="" type="button" onpointerenter="" onpointerleave="" onclick="heroAttack(2)">${hero.attacks[2].name}</button>
+            <button class="standardBG" type="button" onpointerenter="" onpointerleave="" onclick="heroAttack(0)">${hero.attacks[0].name}</button>
+            <button class="standardBG" type="button" onpointerenter="" onpointerleave="" onclick="heroAttack(1)">${hero.attacks[1].name}</button>
+            <button class="standardBG" type="button" onpointerenter="" onpointerleave="" onclick="heroAttack(2)">${hero.attacks[2].name}</button>
         </div>
         <div id="potion-buttons" class="col-xs-3 standardBG">
     `
     for (let pot in hero.potions) {
         let potion = hero.potions[pot];
         template += `
-            <img type="button" onpointerenter="" onpointerleave="" onclick="usePotion('${pot}')" src="${potion.image}" alt="${potion.name}">
+            <button type="button" onpointerenter="" onpointerleave="" onclick="usePotion('${pot}')"><img  src="${potion.image}" alt="${potion.name}"></button>
         `
     }
 
@@ -126,15 +194,18 @@ function drawHeroStatusBar() {
 
 function drawEnemyStatusBar() {
     let template = `
-        <h3>Name: ${currentEnemy.name}</h3>
-        <h3>HP: ${currentEnemy.stats.currentHp}/${currentEnemy.stats.maxHp}</h3>
+        <div id="enemy-stats" class="standardBG">
+            <h4>Name: ${currentEnemy.name}</h4>
+            <p>HP: ${currentEnemy.stats.currentHp}/${currentEnemy.stats.maxHp}</p>
+            <p>ATK: ${currentEnemy.stats.atk}</p>
+            <p>SPD: ${currentEnemy.stats.spd}</p>
+        </div>
     `
 
     document.getElementById('enemy-status-bar').innerHTML = template;
 }
 
 function usePotion(id) {
-    console.log(hero.potions);
     hero.potions[id].isActive = true;
     update();
 }
@@ -153,14 +224,51 @@ function enemyAttack() {
 
 function attack(attacker, attackNum, target) {
     target.stats.currentHp -= attacker.attacks[attackNum].potency * attacker.stats.atk;
-    if (target.stats.currentHp < 0) target.stats.currentHp = 0;
+    if (target.stats.currentHp < 0) {
+        target.stats.currentHp = 0;
+        if (target instanceof Hero) {
+            heroDied();
+        } else {
+            enemyDefeated();
+        }
+    }
 
     update();
 }
 
-function hardReset() {
-    hero.stats.currentHp = hero.stats.maxHp;
+function enemyDefeated() {
+    let enemyNum = enemies.indexOf(currentEnemy);
+    hero.currentExp += currentEnemy.stats.maxHp * currentEnemy.stats.atk * currentEnemy.stats.spd / 100;
+    hero.checkLevel();
+
+    // TODO: Check if you just defeated boss here
     currentEnemy.stats.currentHp = currentEnemy.stats.maxHp;
+
+    if (enemyNum >= enemies.length - 1) {
+        let zoneNum = zones.indexOf(currentZone);
+        // Switch to new zone, or fight boss if last zone
+        if (currentZone >= zones.length - 1) {
+            currentEnemy = boss;
+        } else {
+            currentEnemy = enemies[0];
+            currentZone = zones[zoneNum + 1];
+        }
+    } else {
+        // TODO: Have these be dynamically created enemies instead.
+        currentEnemy = enemies[enemyNum + 1];
+    }
+}
+
+function heroDied() {
+
+}
+
+function hardReset() {
+    // TODO: Should probably have a popup confirmation for this.
+    createHero();
+    currentEnemy.stats.currentHp = currentEnemy.stats.maxHp;
+    currentEnemy = enemies[0];
+    currentZone = zones[0];
 
     update();
 }
