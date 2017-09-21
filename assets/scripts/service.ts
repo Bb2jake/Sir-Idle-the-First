8fcff58b8e6a54e1e5cae17b99e7e7875c8c3e2d
Jake Pogue <bb2jake@gmail.com>
2017-09-20 18:38:54 -0600

    removed vue/backend attempts, changed file structure

--- assets/scripts/service.ts
+++ assets/scripts/service.ts
@@ -377,7 +377,6 @@ class Service {
 		this.hero.potions.forEach(function (potion) {
 			if (potion.isActive) {
 				potion.boostTimeRemaining -= 1 / 30;
-				// TODO: Show seconds left on screen
 				if (potion.boostTimeRemaining <= 0) {
 					potion.boostTimeRemaining = 0;
 					this.deactivatePotion(potion);
